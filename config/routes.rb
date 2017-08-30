9d9922a853059d296cfcee08bb80ed906a2bef7b
Cody Higgins <cody.higginsdev18@gmail.com>
2017-08-30 17:11:54 -0600

    added in static pages needed, took away sign up ablility and gave login logout rights

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
 
 	devise_for :users, path: '', path_names: { sign_in: "login", sign_out: "logout", sign_up: "register"} 
 
-  root to: 'pages#home' 
+  root to: 'pages#home'
+
+  get 'welcome', to: 'pages#welcome'
+
+  get 'info', to: 'pages#info'
+
+  get 'faq', to: 'pages#faq'
 end
